67b0bab3cf21d3d19540169d36e321aed70db43a
Manuel <mhoehn@live.de>
2025-02-21 15:54:50 +0100

    Changed Border to be round

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto p-4 border-2 border-solid rounded-xs mt-5">
+  <div class="container mx-auto p-4 border rounded-xl mt-5">
     <Navbar />
 
     <h1 class="text-3xl font-bold mb-4">Übersicht</h1>
